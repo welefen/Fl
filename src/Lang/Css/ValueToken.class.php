e40a7bf08e1425540eb5f8ad35945ea65406c841
welefen <welefen@gmail.com>
2013-12-13 18:57:07 +0800

    update

--- src/Lang/Css/ValueToken.class.php
+++ src/Lang/Css/ValueToken.class.php
@@ -6,28 +6,55 @@
  *
  */
 class Fl_Css_ValueToken extends Fl_Token {
-
-    /**
-     * 
-     * 当前分析css属性值对应的属性名
-     * @var string
-     */
-    public $property = "";
-
-    /**
-     * 执行
-     * @see Fl_Token::run()
-     */
-    public function run () {
-        
-    }
-
-    /**
-     * 获取下一个token
-     * @see Fl_Token::getNextToken()
-     */
-    public function getNextToken () {
-        //do something
-        //do xxx
-    }
+	
+	/**
+	 * 
+	 * 当前分析css属性值对应的属性名
+	 * @var string
+	 */
+	public $property = "";
+	
+	/**
+	 * 执行
+	 * @see Fl_Token::run()
+	 */
+	public function run() {
+		$tokens = array ();
+		while ( true ) {
+			$token = $this->getNextToken ();
+			if (empty ( $token )) {
+				break;
+			}
+			$tokens [] = $token;
+		}
+		return $tokens;
+	}
+	
+	/**
+	 * 获取下一个token
+	 * @see Fl_Token::getNextToken()
+	 */
+	public function getNextToken() {
+		$token = parent::getNextToken ();
+		if ($token || $token === false) {
+			return $token;
+		}
+		$result = "";
+		while ( $this->pos < $this->length ) {
+			$char = $this->text {$this->pos};
+			if ($this->text {$this->pos + 1} === '(') {
+				$match = $this->getMatched ( "(", ")" );
+				$result .= $char . $match;
+				continue;
+			}
+		
+		}
+	}
+	/**
+	 * 
+	 * 获取token类型
+	 */
+	public function getTokenType() {
+	
+	}
 }
