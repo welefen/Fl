fc845173d60c47dd273f1db6824a63b5753a76c6
welefen <welefen@gmail.com>
2015-03-25 18:06:16 +0800

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh 
 find src/ -type d -name ".svn" | xargs rm -rf;
 find . -type f -name "diff" | xargs rm -rf;
-exit 0;
-scp -r * lichengyin@10.16.15.155:/home/lichengyin/htdocs/compressor/vender/Fl/;
-scp -r * lichengyin@10.16.15.155:/home/lichengyin/htdocs/beautify/vender/Fl/;
-scp -r * welefen@www.kitgram.com:/home/welefen/www/www.flkit.org/;
+scp -r * lichengyin@www.qiwoo.org:/home/q/system/www/qiwu/beautify/vender/Fl;
+scp -r * lichengyin@www.qiwoo.org:/home/q/system/www/qiwu/compressor/vender/Fl;
+scp -r * welefen@www.welefen.com:/home/welefen/www/www.flkit.org/;
